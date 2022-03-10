9bd9c148427ea72f24f196facb39b779579954e5
Ktrane <ktrane9@gmail.com>
2022-03-10 00:45:39 -0500

    Added Sound Effect and Updates to Index and Script

--- script.js
+++ script.js
@@ -1,10 +1,53 @@
 var givenID = 4;
 var lastID = shift(givenID);
+var sound = new Audio();
+sound.src = "OOT_PauseMenu_Select.wav";
+
+var check_array = [];
+initial(check_array);
 
 function change() {
 
     alert("The button was changed.");
     document.getElementById('item4').innerHTML = "Change";
+}
+
+function initial(check_array) {
+    //Creates first four items*
+    for(i = 0; i < 4; i++) {
+        var j = i+1;
+        //var check = document.getElementById("box" + j.toString());
+        var check_prop = {index:i, id:("box" + j.toString()), value:false};
+        check_array[i] = check_prop;
+    }
+    
+}
+
+function search(id) {
+
+    var item = document.getElementById(id);
+    for(i = 0; i < check_array.length; i++) {
+        if(check_array[i].id === item.id) {
+            //return check_array[i].value;
+            return i;
+        }
+    }
+}
+
+function set(i) {
+
+    var j = i + 1;
+
+    if(check_array[i].value == false) {
+        var check_prop = {index:i, id:("box" + j.toString()), value:true};
+        check_array[i] = check_prop;
+    }
+    
+    else {
+        var check_prop = {index:i, id:("box" + j.toString()), value:false};
+        check_array[i] = check_prop;
+    }
+
 }
 
 function user_add() {
@@ -21,6 +64,18 @@ function shift(x) {
     return x;
 }
 
+function play_sound(id) {
+
+    var i = search(id);
+
+    if(check_array[i].value == false) {
+        sound.play();
+        set(i);
+    }
+    else
+        set(i);
+}
+
 function add_item() {
 
     //Creating List Element and Prompting User
@@ -33,6 +88,8 @@ function add_item() {
     var input = document.createElement("input");
     input.setAttribute("type", "checkbox");
     input.id = "box" + id.toString();
+    input.onclick = function() {play_sound(id)};
+    
 
     //Creating Date Box
     var date = document.createElement("input");
@@ -47,6 +104,7 @@ function add_item() {
     var button = document.createElement("button");
     button.id = id.toString();
     button.appendChild(document.createTextNode("Delete"));
+    button.onclick = function() {delete_item(id)};
     element.appendChild(button);
 
     //Creating Break Element
@@ -57,7 +115,8 @@ function add_item() {
     document.querySelector('ul').appendChild(element);
     document.querySelector('ul').appendChild(br);
     //document.querySelector("#" + element.id.toString()).appendChild(button);
-    button.onclick = function() {delete_item(id)};
+    var check_prop = {index:(id-1), id:("box" + id.toString()), value:false};
+    check_array[id-1] = check_prop;
 
     lastID = shift(id);
 }
@@ -69,10 +128,6 @@ function delete_item(id) {
     item.remove();
     br.remove();
 }
-
-/*function initial() {
-    //Creates first four items*
-}*/
 
 /*function check(id) {
     id = id.charAt(3);
