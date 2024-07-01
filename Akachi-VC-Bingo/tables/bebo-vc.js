157d5874c4c78a47eed89b0b737324edc12548d9
Ktrane <43693004+ktrane9@users.noreply.github.com>
2024-07-01 02:36:53 -0400

    Bingo Board Upload

---
+++ Akachi-VC-Bingo/tables/bebo-vc.js
@@ -0,0 +1,234 @@
+var bingoList = [];
+
+bingoList[1] = [
+    { name: "Bebachi goes to Smoke", types: ["Bebachi"] },
+    { name: "Sonic talk to Fam", types: ["Sonic"] },
+    { name: "Spongebob reference", types: ["E 4 Everyone"] },
+    { name: "Vector streams a game literally NOBODY cares about", types: ["Vector"] },
+    { name: "Kippmin says \"DIE\"", types: ["Kippmin"] },
+    { name: "Vector coughs", types: ["Vector"] },
+    { name: "I am being ATTACKED", types: ["Dor"] },
+    { name: "\"I'M SCREAMINNNG\"", types: ["JD"] }
+];
+bingoList[2] = [
+    { name: "Chas randomly starts rapping", types: ["Chas"] },
+    { name: "Sonic saying falsehoods as if they were true", types: ["Sonic"] },
+    { name: "Milky gets interrupted mid convo", types: ["Milky", "E 4 Everyone"] },
+    { name: "Someone doesn't answer the question", types: ["E 4 Everyone"] },
+    { name: "Vector rambling about something nobody knows about", types: ["Vector"] },
+    { name: "Dor bullying ensues", types: ["Dor","E 4 Everyone"] },
+    { name: "Everyone gets mad at Sonic mid-game", types: ["Sonic","E 4 Everyone"] },
+    { name: "JD saying \"WHAAAT\"", types: ["JD"] }
+];
+bingoList[3] = [
+    { name: "Milky talks to her dogs", types: ["Milky"] },
+    { name: "Dor saying \"Listen\"", types: ["Dor"] },
+    { name: "Big/Round/Wide", types: ["E 4 Everyone"] },
+    { name: "Vector says \"You can't say that\"", types: ["Vector"] },
+    { name: "I'm gonna kill myself!", types: ["E 4 Everyone"] },
+    { name: "PERIOD", types: ["JD","Milky","E 4 Everyone"] },
+    { name: "\"haiii...\" \(Sonic joined\)", types: ["Sonic"] },
+    { name: "Kippmin making weird af guttural noises", types: ["Kippmin"] }
+];
+bingoList[4] = [
+    { name: "JD references Beyonce", types: ["JD"] },
+    { name: "Sonic starts back peddling", types: ["Sonic"] },
+    { name: "Bebachi starts gaslighting", types: ["Bebachi"] },
+    { name: "\"Look, it's Chas!\"", types: ["E 4 Everyone"] },
+    { name: "Bebachi yawn", types: ["Bebachi"] },
+    { name: "\"YEOWCH!!\"", types: ["Milky"] },
+    { name: "Chas screams", types: ["Chas"] },
+    { name: "Bebachi streaming a blank screen", types: ["Bebachi"] }
+];
+bingoList[5] = [
+    { name: "Bebachi saying Good Morning anywhere before 6am and after 12pm", types: ["Bebachi"] },
+    { name: "JD's Cackle", types: ["JD"] },
+    { name: "More than 2 streams going on at once", types: ["E 4 Everyone"] },
+    { name: "\"Does anybody want to play/watch X before I Y\"", types: ["Bebachi"] },
+    { name: "Dor cackle", types: ["Dor"] },
+    { name: "Kill Yourself!", types: ["E 4 Everyone"] },
+    { name: "Chas Time", types: ["Chas"] },
+    { name: "Bebachi streaming nothing overnight \(He fell asleep\)", types: ["Bebachi"] }
+];
+bingoList[6] = [
+    { name: "Vector/Philo/Chas old joke", types: ["E 4 Everyone"] },
+    { name: "Soundboard Spam", types: ["E 4 Everyone"] },
+    { name: "\"That's the Chas Effect\"", types: ["Chas"] },
+    { name: "Sonic high pitched scream", types: ["Sonic"] },
+    { name: "Schmuck starts watching something mid art stream", types: ["Schmuck"] },
+    { name: "Mario Kart discourse", types: ["Mario Kart","E 4 Everyone"] },
+    { name: "Dor says \"Look\"", types: ["Dor","E 4 Everyone"] },
+    { name: "Ktrane calling someone a cocksucker", types: ["Ktrane"] }
+];
+bingoList[7] = [
+    { name: "Chas saying \"WHAT\"", types: ["Chas"] },
+    { name: "Max being horny", types: ["Max"] },
+    { name: "Max giggling in a husky tone", types: ["Max"] },
+    { name: "Dor talking about pups", types: ["Dor"] },
+    { name: "Schmuck \"HUH\"", types: ["Schmuck"] },
+    { name: "Kippmin disconnects and says goodnight but comes back within the hour", types: ["Kippmin"] },
+    { name: "\"HEY YALL\"", types: ["Mattie"] },
+    { name: "Ktrane calling someone a cunt", types: ["Ktrane"] }
+];
+bingoList[8] = [
+    { name: "Mattie AFK", types: ["Mattie"] },
+    { name: "Nicki Reference", types: ["E 4 Everyone"] },
+    { name: "Bebachi Rap Ramble", types: ["Bebachi"] },
+    { name: "Parroting Occurs", types: ["Sonic"] },
+    { name: "\"Nande\"", types: ["Milky"] },
+    { name: "Chas gasp", types: ["Chas"] },
+    { name: "Schmuckernet", types: ["Schmuck","Bebachi","Mario Kart"] },
+    { name: "I DEid", types: ["Schmuck"] }
+];
+bingoList[9] = [
+    { name: "Karaoke Night for no reason", types: ["E 4 Everyone"] },
+    { name: "Mattie fart sfx", types: [""] },
+    { name: "Mattie ", types: [""] },
+    { name: "More than 2 streams going on at once", types: ["E 4 Everyone"] },
+    { name: "Korny Ktrane", types: ["Ktrane"] },
+    { name: "Zeke being aggressive", types: ["Zeke"] },
+    { name: "Zeke spiels about game factoids", types: ["Zeke"] },
+    { name: "\"Biracial\"", types: ["E 4 Everyone"] }
+];
+bingoList[10] = [
+    { name: "JD actually says goodnight before leaving VC", types: ["JD"] },
+    { name: "Schmuck R&B ramble", types: ["Schmuck"] },
+    { name: "Schmuck animal facts PSA", types: ["Schmuck"] },
+    { name: "Nyan starts snoring", types: ["Nyan"] },
+    { name: "Azalea Banks", types: ["E 4 Everyone"] },
+    { name: "Mattie bitches about Overwatch", types: ["Mattie"] },
+    { name: "\"I sorry\"", types: ["Mattie"] },
+    { name: "One person laughs at Bebachi comment nobody but them understand and everyone starts laughing", types: ["Bebachi","E 4 Everyone"] }
+];
+bingoList[11] = [
+    { name: "Bebachi laying into Mattie for the TINIEST reason", types: ["Bebachi","Mattie"] },
+    { name: "Zeke rolling Non-Spanish Rs", types: ["Zeke"] },
+    { name: "Milky says \"It's my first win!\"", types: ["Milky"] },
+    { name: "Dor teaching \"Trade\"", types: ["Dor"] },
+    { name: "Ktrane locks in", types: ["Ktrane"] },
+    { name: "Sonic cheats at Mario Kart", types: ["Sonic"] },
+    { name: "Mattie says \"Ok Y'all I'll be back 10 minutes\"", types: ["Mattie"] }
+];
+bingoList[12] = [
+    { name: "Ktrane being mad", types: ["Ktrane"] },
+    { name: "ICE SPICE MENTIONS", types: ["E 4 Everyone"] },
+    { name: "\"MOVE POPPY\"", types: ["Bebachi"] },
+    { name: "Someone who was muted for 20+ minutes thinking they were talking", types: ["Ktrane","Schmuck","Max"] },
+    { name: "IJBOL", types: ["JD","Milky"] },
+    { name: "Chas trying to ride on Ktrane online", types: ["Chas", "Ktrane"] },
+    { name: "Max glazing art", types: ["Max"] }
+];
+bingoList[13] = [
+    { name: "Everyone being mean for no reason", types: ["E 4 Everyone"] },
+    { name: "A REALLY good impersonation happens", types: ["E 4 Everyone"] },
+    { name: "Max horse gif", types: ["Max"] },
+    { name: "Milky youtube dinner combo", types: ["Milky"] },
+    { name: "Bebachi talking about how Zeke is bagging in MK", types: ["Bebachi","Zeke","Mario Kart"] },
+    { name: "Drake", types: ["E 4 Everyone"] },
+    { name: "\"I fucking hate you!\"", types: ["Milky"] }
+];
+bingoList[14] = [
+    { name: "Ktrane' TV in the back", types: ["Ktrane"] },
+    { name: "Philo being pregnant", types: ["Philo"] },
+    { name: "Dor rping with Max/Kippmin", types: ["Dor","Max","Kippmin"] },
+    { name: "JD disconnect someone", types: ["JD","Kippmin"] },
+    { name: "Milky/JD hating on Daisy", types: ["JD","Sonic","Milky"] },
+    { name: "Ktrane playing a Ktrane game", types: ["Ktrane"] },
+    { name: "Zeke playing Splatoon", types: ["Zeke"] }
+];
+bingoList[15] = [
+    { name: "Lizzo reference in Jackbox", types: ["E 4 Everyone","Jackbox"] },
+    { name: "Ice Spice reference", types: ["E 4 Everyone"] },
+    { name: "Chas being mean", types: ["Chas"] },
+    { name: "Max saying \"WHOA\"", types: ["Max"] },
+    { name: "Ktrane gives you the silent treatment", types: ["Ktrane","E 4 Everyone"] },
+    { name: "Max the Penis Detective", types: ["Max"] },
+    { name: "\"That's Not...\"", types: ["JD","Milky"] }
+];
+bingoList[16] = [
+    { name: "Tokyo Toni Reference", types: ["E 4 Everyone"] },
+    { name: "\"Big body bitches\"", types: ["Milky","Vector","Mario Kart"] },
+    { name: "Mario Kart chooses one person's stages in a row and NO ONE ELSE", types: ["Mario Kart","E 4 Everyone"] },
+    { name: "Music video watch party", types: ["Milky","Schmuck","Chas","E 4 Everyone"] },
+    { name: "Nyan gets talked over", types: ["Nyan"] },
+    { name: "Kippmin gets depressed", types: ["Kippmin"] },
+    { name: "Kippmin not understanding something after it was just explained", types: ["Kippmin","E 4 Everyone"] }
+];
+bingoList[17] = [
+    { name: "Game gets pinged but only 2 people join", types: ["E 4 Everyone", "Jackbox", "Mario Kart"] },
+    { name: "Pokemon Discussion", types: ["Schmuck","Zeke","Vector","Dor","Ktrane"] },
+    { name: "Chas asking anyone if they've listened to a certain album", types: ["Chas","JD","Schmuck","Bebachi"] },
+    { name: "Unnecessary race discussion/politics", types: ["E 4 Everyone"] },
+    { name: "\"Well...\"", types: ["Milky"] },
+    { name: "Anyone getting mad at videogame and leaves", types: ["Ktrane","Milky","Jackbox","Mario Kart"] },
+    { name: "Chat is a 50/50 split", types: ["E 4 Everyone"] }
+];
+bingoList[18] = [
+    { name: "Kaito joins vc frame 1 after Bebachi", types: ["E 4 Everyone"] },
+    { name: "Chas white lies", types: ["Chas"] },
+    { name: "Chas claims he doesn't lie", types: ["Chas"] },
+    { name: "Philo's blue eyes are mentioned", types: ["Philo","E 4 Everyone"] },
+    { name: "\"But I'm a girl\"", types: ["Milky"] },
+    { name: "Schmuck gets flamed for being Bisexual, Ktrane avoids it somehow", types: ["Schmuck","Bebachi"] },
+    { name: "Lightskin mentioned", types: ["Bebachi"] }
+];
+bingoList[19] = [
+    { name: "\"Okay, I don't have to take this from you.\"", types: ["Philo","Bebachi","E 4 Everyone"] },
+    { name: "Someone new in the VC overshares", types: ["E 4 Everyone"] },
+    { name: "Pretentious \"Hmmmmm\" reference", types: ["E 4 Everyone"] },
+    { name: "Chas mic static", types: ["Chas"] },
+    { name: "Sonic bullying ensues", types: ["Sonic","E 4 Everyone"] },
+    { name: "Zeke excuses", types: ["Zeke"] },
+    { name: "Bebachi vigorously agreeing in the convo because he's annoyed with your answer", types: ["Bebachi","E 4 Everyone"] }
+];
+bingoList[20] = [
+    { name: "Ktrane viscerally annihilating someone verbally", types: ["Ktrane"] },
+    { name: "Kippmin picks Baby Park", types: ["Kippmin"] },
+    { name: "\"That's so Interesting...\"", types: ["JD"] },
+    { name: "Akachi says \"I fucking hate you\"", types: ["Bebachi", "E 4 Everyone"] },
+    { name: "\"Lemme ask the audience\"", types: ["Milky"] },
+    { name: "RPG Discussion", types: ["Vector","Ktrane"] },
+    { name: "\"Meow...\"", types: ["Dor"] }
+];
+bingoList[21] = [
+    { name: "Nyan talking about Max's cock", types: ["Nyan","Max"] },
+    { name: "\"Get that out of my EAR!\"", types: ["Milky","Dor"] },
+    { name: "Vector makes a new word", types: ["Vector"] },
+    { name: "Bebachi saying that Zeke is glazing someone in game", types: ["Bebachi","Zeke","Mario Kart"] },
+    { name: "A Champed Up abomination is made", types: ["Jackbox","E 4 Everyone"] },
+    { name: "\"That's so real\"", types: ["Milky","E 4 Everyone"] }
+];
+bingoList[22] = [
+    { name: "Benny/Consume Burger", types: ["Benny"] },
+    { name: "Sonic/Bebachi/Vector are the last ones in vc", types: ["Sonic","Bebachi","Vector"] },
+    { name: "Nyan roasts the fuck out of someone", types: ["Nyan","E 4 Everyone"] },
+    { name: "Sonic speaks in fake Spanish", types: ["Sonic","Milky"] },
+    { name: "Sonic is paranoid of Vector in MK", types: ["Mario Kart","Sonic","Vector"] },
+    { name: "\"You were in my way!\" \(Had the tiniest reason to use his item on you\)", types: ["Vector","Mario Kart"] }
+];
+bingoList[23] = [
+    { name: "Duolingo mentioned", types: ["Bebachi","Ktrane"] },
+    { name: "Ktrane's anger makes everyone laugh", types: ["Ktrane","E 4 Everyone"] },
+    { name: "Ktrane's anger makes everyone scared", types: ["Ktrane","E 4 Everyone"] },
+    { name: "Someone speaks without thinking", types: ["E 4 Everyone"] },
+    { name: "Thinking something but you will not say it", types: ["E 4 Everyone"] },
+    { name: "People are in the VC before 12pm EST", types: ["E 4 Everyone"] }
+];
+bingoList[24] = [
+    { name: "Chas snoring", types: ["Chas"] },
+    { name: "Milky and Sonic being on the same page for once", types: ["Milky","Sonic"] },
+    { name: "Group Twitter Deep Dive", types: ["Schmuck","Bebachi","E 4 Everyone"] },
+    { name: "1", types: ["E 4 Everyone"] },
+    { name: "2", types: ["E 4 Everyone"] },
+    { name: "3", types: ["E 4 Everyone"] }
+];
+bingoList[25] = [
+    { name: "Gabu joins VC", types: ["Gabu"] },
+    { name: "Trusty joins VC", types: ["Trusty"] },
+    { name: "Benny joins VC", types: ["Benny"] },
+    { name: "Ktrane flirting with Chas/talking about something horny that was in DMs", types: ["Ktrane","Chas"] },
+    { name: "Mario Kart is played without incident", types: ["Mario Kart","E 4 Everyone"] },
+    { name: "\"Alexa play this song\" and JD's ALexa actually plys it", types: ["JD","Chas"] }
+];
+
+$(function() { srl.bingo(bingoList, 5); });
